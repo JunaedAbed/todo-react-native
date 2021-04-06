7272d8a5b87790671c23f0e1e09ed74e62074926
Junaed Abed <junaedccpc17@gmail.com>
2021-04-07 01:09:47 +0600

    modified:   App.js

--- App.js
+++ App.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import {
+  KeyboardAvoidingView,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import Task from "./components/Task";
 
 export default function App() {
@@ -15,6 +23,22 @@ export default function App() {
           <Task text={"Task 2"} />
         </View>
       </View>
+
+      {/* write a task */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Start writing a task..."}
+        />
+        <TouchableOpacity>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </View>
   );
 }
@@ -35,4 +59,29 @@ const styles = StyleSheet.create({
   items: {
     marginTop: 25,
   },
+  writeTaskWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    paddingLeft: 10,
+    width: 250,
+    backgroundColor: "white",
+    borderRadius: 60,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: "#fff",
+    borderRadius: 60,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addText: {},
 });
